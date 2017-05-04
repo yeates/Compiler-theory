c8e633a8fc0dea293b93533e29fad5c363e2c5d2
yeates <yeates98@hotmail.com>
2017-05-04 22:06:06 +0800

    v0.8
    大跃进

--- TEST Compiler/Parser.cpp
+++ TEST Compiler/Parser.cpp
@@ -2,26 +2,46 @@
 
 //语法分析器类的构造函数
 PARSER::PARSER(){
-    first_declaration_stat.push_back("int");
+    /*FIRST集合*/
+    first_program.push_back("(");
     first_declaration_list.push_back("int");
     first_declaration_list.push_back("_E");    //_E表示ε
-    first_program.push_back("(");
+    first_declaration_stat.push_back("int");
+    first_statement.push_back("if");
+    first_statement.push_back("while");
+    first_statement.push_back("for");
+    first_statement.push_back("write");
+    first_statement.push_back("read");
+    first_statement.push_back("{");
+    first_statement.push_back("_ID");
+    first_statement.push_back("_NUM");
+    first_statement.push_back("(");
+    first_statement.push_back(";");
+    first_statement_list.push_back("if");
+    first_statement_list.push_back("while");
+    first_statement_list.push_back("for");
+    first_statement_list.push_back("write");
+    first_statement_list.push_back("read");
+    first_statement_list.push_back("{");
+    first_statement_list.push_back("_ID");
+    first_statement_list.push_back("_NUM");
+    first_statement_list.push_back("(");
+    first_statement_list.push_back(";");
+    first_statement_list.push_back("_E");
     first_if_stat.push_back("if");
-    first_tmp_if_stat.push_back("_E");
-    first_tmp_if_stat.push_back("else");
     first_while_stat.push_back("while");
     first_for_stat.push_back("for");
     first_write_stat.push_back("write");
     first_read_stat.push_back("read");
     first_compound_stat.push_back("{");
-    first_expression_stat.push_back("_ID");     //TODO：没推出来
-    first_expression_stat.push_back("_NUM");     //TODO：没推出来
-    first_expression_stat.push_back("(");     //TODO：没推出来
-    first_expression_stat.push_back(";");     //TODO：没推出来
-    first_expression.push_back("_ID");//TODO：补全
-    first_expression.push_back("_NUM");//TODO：补全
-    first_expression.push_back("(");//TODO：补全
-    first_factor.push_back("_ID");      //表示
+    first_expression_stat.push_back("_ID");
+    first_expression_stat.push_back("_NUM");
+    first_expression_stat.push_back("(");
+    first_expression_stat.push_back(";");
+    first_expression.push_back("_ID");
+    first_expression.push_back("_NUM");
+    first_expression.push_back("(");
+    first_factor.push_back("_ID");
     first_factor.push_back("_NUM");
     first_factor.push_back("(");
     first_tmp_term.push_back("*");
@@ -46,49 +66,19 @@ PARSER::PARSER(){
     first_bool_expr.push_back("_ID");
     first_bool_expr.push_back("_NUM");
     first_bool_expr.push_back("(");
-    first_statement.push_back("if");    //TODO:补全
-    first_statement.push_back("while");
-    first_statement.push_back("for");
-    first_statement.push_back("write");
-    first_statement.push_back("read");
-    first_statement.push_back("{");
-    first_statement.push_back("_ID");
-    first_statement.push_back("_NUM");
-    first_statement.push_back("(");
-    first_statement.push_back(";");
-    first_statement_list.push_back("if");
-    first_statement_list.push_back("while");
-    first_statement_list.push_back("for");
-    first_statement_list.push_back("write");
-    first_statement_list.push_back("read");
-    first_statement_list.push_back("{");
-    first_statement_list.push_back("_ID");
-    first_statement_list.push_back("_NUM");
-    first_statement_list.push_back("(");
-    first_statement_list.push_back(";");
-    first_statement_list.push_back("_E");
 
+    /*FOLLOW集合*/
     follow_declaration_list.push_back("if");
     follow_declaration_list.push_back("while");
     follow_declaration_list.push_back("for");
     follow_declaration_list.push_back("write");
     follow_declaration_list.push_back("read");
     follow_declaration_list.push_back("{");
+    follow_declaration_list.push_back("}");
     follow_declaration_list.push_back("_ID");
     follow_declaration_list.push_back("_NUM");
     follow_declaration_list.push_back("(");
     follow_declaration_list.push_back(";");
-    follow_declaration_list.push_back("}");
-    follow_tmp_if_stat.push_back("if");
-    follow_tmp_if_stat.push_back("while");
-    follow_tmp_if_stat.push_back("for");
-    follow_tmp_if_stat.push_back("write");
-    follow_tmp_if_stat.push_back("read");
-    follow_tmp_if_stat.push_back("{");
-    follow_tmp_if_stat.push_back("_ID");
-    follow_tmp_if_stat.push_back("_NUM");
-    follow_tmp_if_stat.push_back("(");
-    follow_tmp_if_stat.push_back(";");
     follow_statement_list.push_back("}");
     follow_tmp_bool_expr.push_back(";");
     follow_tmp_bool_expr.push_back(")");
@@ -110,6 +100,20 @@ PARSER::PARSER(){
     follow_tmp_term.push_back(">=");
     follow_tmp_term.push_back("==");
     follow_tmp_term.push_back("!=");
+    follow_expression.push_back(")");
+    follow_expression.push_back(";");
+    follow_bool_expr.push_back(";");
+    follow_bool_expr.push_back(")");
+    follow_term.push_back(";");
+    follow_term.push_back(")");
+    follow_term.push_back("+");
+    follow_term.push_back("-");
+    follow_term.push_back(">");
+    follow_term.push_back("<");
+    follow_term.push_back("<=");
+    follow_term.push_back(">=");
+    follow_term.push_back("==");
+    follow_term.push_back("!=");
 
 
     swi_wrong_state = false;
@@ -125,7 +129,8 @@ int PARSER::f_program(int no){
     }
     no = f_declaration_list(no);
     no = f_statement_list(no);
-    ch = GetNextSymbol(no);
+    if(no >= LA.m_str.size())   return no;
+    ch = LA.m_str[no];
     if(ch == "}"){
         return ++no;
     }
@@ -136,15 +141,6 @@ int PARSER::f_program(int no){
 }
 //#2
 int PARSER::f_declaration_list(int no){
-//    //当出现错误时的错误处理
-//    while(swi_wrong_state == true){
-//        //如果能进入下一个状态说明，当前字符是正确的
-//        if(!is_include(no, first_declaration_stat) && is_include(no, follow_declaration_list)){
-//            swi_wrong_state = false;
-//        }
-//        else if(is_include(no, first_declaration_stat)) swi_wrong_state = false;
-//        else no++;
-//    }
     if(!is_include(no, first_declaration_stat) && is_include(no, follow_declaration_list)){
         return no;
     }
@@ -160,7 +156,7 @@ int PARSER::f_declaration_stat(int no){
     if(ch == "int"){
         ch = LA.m_str[++no];
     }
-    else    return no = error_handle(no);
+    else    no = error_handle(no);
     if(LA.m_str_attribute[no] == 1){
         ch = LA.m_str[++no];
     }
@@ -171,10 +167,7 @@ int PARSER::f_declaration_stat(int no){
         ch = LA.m_str[no];
     }
     if(ch == ";")   return ++no;
-    else{
-        cout << "在行号：" << LA.m_str_lineNum[no-1] << "，符号：\"" << LA.m_str[no-1] << "\" 的后方，缺失符号: \";\"" << endl;
-        return no;
-    }
+    else    return no = defect_handle(no, ";");
 }
 //#4
 int PARSER::f_statement_list(int no){
@@ -213,31 +206,36 @@ int PARSER::f_statement(int no){
     else    return no = error_handle(no);
     return no;
 }
-//#6
+//#6：判断完 if (<expression>) <statement > 后,判断是否下一个字符为else，是则继续判断else <statement>，不是则跳出
 int PARSER::f_if_stat(int no){
     string ch = LA.m_str[no];
     if(ch == "if")  ch = LA.m_str[++no];
-    else    return no = error_handle(no);
+    else    no = error_handle(no);
     if(ch == "(")  ch = LA.m_str[++no];
-    else    return no = error_handle(no);
+    else    no = defect_handle(no, "(");
     no = f_expression(no);
+    ch = LA.m_str[no];
     if(ch == ")")  ch = LA.m_str[++no];
-    else    return no = error_handle(no);
+    else    no = defect_handle(no, ")");
     no = f_statement(no);
-    no = f_tmp_if_stat(no);
+    ch = LA.m_str[no];
+    if(ch == "else"){
+        no++;
+        no = f_statement(no);
+    }
     return no;
 }
 //#7
 int PARSER::f_while_stat(int no){
     string ch = LA.m_str[no];
     if(ch == "while")   ch = LA.m_str[++no];
-    else    return no = error_handle(no);
+    else    no = error_handle(no);
     if(ch == "(")   ch = LA.m_str[++no];
-    else    return no = error_handle(no);
+    else    no = defect_handle(no, "(");
     no = f_expression(no);
     ch = LA.m_str[no];
     if(ch == ")")   ch = LA.m_str[++no];
-    else    return no = error_handle(no);
+    else    no = defect_handle(no, ")");
     no = f_statement(no);
     return no;
 }
@@ -245,18 +243,21 @@ int PARSER::f_while_stat(int no){
 int PARSER::f_for_stat(int no){
     string ch = LA.m_str[no];
     if(ch == "for") ch = LA.m_str[++no];
-    else    return no = error_handle(no);
+    else    no = error_handle(no);
     if(ch == "(")   ch = LA.m_str[++no];
-    else    return no = error_handle(no);
+    else    no = defect_handle(no, "(");
     no = f_expression(no);
     ch = LA.m_str[no];
     if(ch == ";")   ch = LA.m_str[++no];
+    else    no = defect_handle(no, ";");
     no = f_expression(no);
     ch = LA.m_str[no];
     if(ch == ";")   ch = LA.m_str[++no];
+    else    no = defect_handle(no, ";");
     no = f_expression(no);
     ch = LA.m_str[no];
     if(ch == ")")   ch = LA.m_str[++no];
+    else    no = defect_handle(no, ")");
     no = f_statement(no);
     return no;
 }
@@ -264,50 +265,51 @@ int PARSER::f_for_stat(int no){
 int PARSER::f_write_stat(int no){
     string ch = LA.m_str[no];
     if(ch == "write")   ch = LA.m_str[++no];
-    else    return no = error_handle(no);
+    else    no = error_handle(no);
     no = f_expression(no);
     ch = LA.m_str[no];
     if(ch == ";")   return ++no;
-    else    return no = error_handle(no);
+    else    return no = defect_handle(no, ";");
 }
 //#10
 int PARSER::f_read_stat(int no){
     string ch = LA.m_str[no];
     if(ch == "read")    ch = LA.m_str[++no];
-    else    return no = error_handle(no);
+    else    no = error_handle(no);
     if(LA.m_str_attribute[no] == 1) ch = LA.m_str[++no];
-    else    return no = error_handle(no);
+    else    no = error_handle(no);
     if(ch == ";")   return ++no;
-    else    return no = error_handle(no);
+    else    return no = defect_handle(no, ";");
 }
 //#11
 int PARSER::f_compound_stat(int no){
     string ch = LA.m_str[no];
     if(ch == "{")   ch = LA.m_str[++no];
-    else    return no = error_handle(no);
+    else    no = defect_handle(no, "{");
     no = f_statement_list(no);
+    if(no >= LA.m_str.size())   return no;
     ch = LA.m_str[no];
     if(ch == "}")   return ++no;
-    else    return no = error_handle(no);
+    else    return no = defect_handle(no, "}");
 }
 //#12
-int PARSER::f_expression_stat(int no){      //TODO
+int PARSER::f_expression_stat(int no){
     string ch = LA.m_str[no];
     if(ch == ";")   return ++no;
     else{
         no = f_expression(no);
         ch = LA.m_str[no];
         if(ch == ";")   return ++no;
-        else return no = error_handle(no);
+        else return no = defect_handle(no, ";");
     }
 }
 //#13
-int PARSER::f_expression(int no){       //TODO
+int PARSER::f_expression(int no){
     string ch = LA.m_str[no];
     if(LA.m_str_attribute[no] == 1 && LA.m_str[no+1] == "="){
         ch = LA.m_str[++no];
         ch = LA.m_str[++no];
-        no =f_bool_expr(no);
+        no = f_bool_expr(no);
         return no;
     }
     else{
@@ -324,15 +326,13 @@ int PARSER::f_bool_expr(int no){
 //#15
 int PARSER::f_tmp_bool_expr(int no){
     string ch = LA.m_str[no];
-    if(is_include(no, follow_tmp_bool_expr)){
-        return no;
-    }
-
     if(ch == ">" || ch == "<" || ch == ">=" || ch == "<=" || ch == "==" || ch == "!="){
         ch = LA.m_str[++no];
+        no = f_additive_expr(no);
     }
-    else    return no = error_handle(no);
-    no = f_additive_expr(no);
+    //如果ch不在first集中，在follow中又能找到包含关系，那么return
+    else if(is_include(no, follow_tmp_bool_expr))   return no;
+    else    return no;
     return no;
 }
 //#16
@@ -350,7 +350,7 @@ int PARSER::f_tmp_additive_expr(int no){
         no = f_tmp_additive_expr(no);
     }
     else if(is_include(no, follow_tmp_additive_expr))   return no;
-    else    return no = error_handle(no);
+    else    return no;      //这儿直接返回，很重要：不能使用error_handle函数，因为error_handle函数会让no++
     return no;
 }
 //#18
@@ -368,31 +368,27 @@ int PARSER::f_tmp_term(int no){
         no = f_tmp_term(no);
     }
     else if(is_include(no, follow_tmp_term))    return no;
-    else    return no = error_handle(no);
+    else    return no;
     return no;
 }
 //#20
 int PARSER::f_factor(int no){
     string ch = LA.m_str[no];
+    //这里不符合改造的文法，但这是为了错误处理，即这只会在输入代码错误的情况下才会发生，所以特殊情况特殊处理
+    //如果ch不属于FIRST集时 return
+    if(!is_include(no, first_factor))   return no;
+    //当ch为ID或者NUM时，return
     if(LA.m_str_attribute[no] == 1 || LA.m_str_attribute[no] == 3)
         return ++no;
     if(ch == "(")   ch = LA.m_str[++no];
-    else    return no = error_handle(no);
+    else    no = defect_handle(no, "(");
     no = f_expression(no);
     ch = LA.m_str[no];
     if(ch == ")")   return ++no;
-    else    return no = error_handle(no);
+    else    return no = defect_handle(no, ")");
 }
-//#21
-int PARSER::f_tmp_if_stat(int no){
-    string ch = LA.m_str[no];
-    if(ch == "else"){
-        ch = LA.m_str[++no];
-        no = f_statement(no);
-        return no;
-    }
-    else    return no;
-}
+
+//相当于数学意义上的属于
 bool PARSER::is_include(int no, vector<string>first){
     if(no >= LA.m_str.size())   return true;    //如果序号no大于了容器的宽度，那么返回
 
@@ -410,17 +406,20 @@ bool PARSER::is_include(int no, vector<string>first){
     }
     return false;
 }
+
 int PARSER::error_handle(int no){
-    swi_wrong_state = true;
+    //swi_wrong_state = true;
     if(no >= LA.m_str.size())   return LA.m_str.size()-1;
         cout << "在行号：" << LA.m_str_lineNum[no] << "，存在语法错误代码\"" << LA.m_str[no] << "\"" << endl;
+    return ++no;
+}
+
+int PARSER::defect_handle(int no, string str){
+    if(no >= LA.m_str.size())   return LA.m_str.size()-1;
+        cout << "在行号：" << LA.m_str_lineNum[no] << "，上的单词：\"" << LA.m_str[no] << "\"的前面缺失了一个符号：\"" << str << "\"" << endl;
     return no;
 }
 
-string PARSER::GetNextSymbol(int no){
-    if(no >= LA.m_str.size())   return "_W";
-    else return LA.m_str[no];
-}
 
 void PARSER::grammar_analysis(){
     LA.text_analysis();     //词法分析器处理单词
--- TEST Compiler/Parser.h
+++ TEST Compiler/Parser.h
@@ -28,9 +28,11 @@ private:
     vector<string>follow_declaration_list;
     vector<string>follow_statement_list;
     vector<string>follow_tmp_bool_expr;
-    vector<string>follow_tmp_if_stat;
     vector<string>follow_tmp_additive_expr;
     vector<string>follow_tmp_term;
+    vector<string>follow_expression;
+    vector<string>follow_bool_expr;
+    vector<string>follow_term;
 
 //----------------------------------------//
 //--------各个非终结符的函数---------------//
@@ -54,13 +56,12 @@ private:
     int f_term(int no);
     int f_tmp_term(int no);
     int f_factor(int no);
-    int f_tmp_if_stat(int no);
 //----------------------------------------//
     bool is_include(int no, vector<string>first);
     int error_handle(int no);
+    int defect_handle(int no, string str);
     bool swi_wrong_state;   //判断现在是否是在错误状态
     string wrong_string;    //出现错误时，失配的字符串。
-    string GetNextSymbol(int no);
 public:
     PARSER();
     void grammar_analysis();
